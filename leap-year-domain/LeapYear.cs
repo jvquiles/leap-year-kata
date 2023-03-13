3b6468d659aebeb445f1e9bd698e894ff0852eb9
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-13 12:34:14 +0100

    feature: implement divisible per 400 use case

--- leap-year-domain/LeapYear.cs
+++ leap-year-domain/LeapYear.cs
@@ -4,7 +4,7 @@ public static class LeapYear
 {
     public static bool IsLeap(int year)
     {
-        if (year % 100 == 0)
+        if (year % 100 == 0 && year % 400 != 0)
         {
             return false;
         }
